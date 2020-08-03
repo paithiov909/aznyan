36d906c3f5514f729f03f28f37f236df575b355b
Kato Akiru <am11.44@gmail.com>
2020-08-03 09:00:10 +0900

    init

---
+++ R/aznyan.R
@@ -0,0 +1,5 @@
+#' aznyan: An Annict GraphQL API Client
+#' @docType package
+#' @name aznyan
+#' @keywords internal
+"_PACKAGE"
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(aznyan)
+
+test_check("aznyan")
