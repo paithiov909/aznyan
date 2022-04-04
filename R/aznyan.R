69960d0b1549a666ec07bb954b170fae3d064ec6
paithiov909 <am11.44@gmail.com>
2022-04-04 21:47:14 +0900

    fix: get_lyrics now writes a csv directly

--- R/aznyan.R
+++ R/aznyan.R
@@ -62,44 +62,47 @@ get_lyrics_list <- function(id,
 #' Scrape lyrics from list
 #'
 #' @param df A tibble that comes of \code{get_lyrics_list}.
+#' @param file String; file name to append lyrircs.
 #' @param links String; column name of lyrics links.
-#' @return tibble.
+#' @return `file` is returned invisibly.
 #' @export
-get_lyrics <- function(df, links = "link") {
+get_lyrics <- function(df, file, links = "link") {
   base_url <- "https://www.uta-net.com"
   links <- dplyr::pull(df, {{ links }})
 
   url <- paste(base_url, links, sep = "/")
 
   session <- polite::bow(base_url, force = FALSE)
-  lyrics <-
-    purrr::map_dfr(url, function(q) {
-      html <- session %>%
-        polite::nod(q) %>%
-        polite::scrape()
-      lyric_body <- html %>%
-        rvest::html_element("#kashi_area") %>%
-        rvest::html_text2()
-      info <- html %>%
-        rvest::html_element(".song-infoboard") %>%
-        rvest::html_element(".detail") %>%
-        rvest::html_text() %>%
-        stringr::str_split("\\n") %>%
-        unlist() %>%
-        purrr::pluck(4) %>%
-        stringr::str_extract_all(pattern = "[\\d,/]+") %>%
-        unlist()
-      data.frame(
-        lyric = lyric_body,
-        released = info[1],
-        page_viewed = info[2]
-      )
-    }) %>%
-    dplyr::mutate(
-      released = lubridate::as_date(.data$released),
-      page_viewed = stringr::str_remove_all(.data$page_viewed, ",") %>%
-        unlist() %>%
-        as.numeric()
-    )
-  data.frame(df, lyrics)
+  purrr::iwalk(url, function(q, itr) {
+    html <- session %>%
+      polite::nod(q) %>%
+      polite::scrape()
+    lyric_body <- html %>%
+      rvest::html_element("#kashi_area") %>%
+      rvest::html_text2()
+    info <- html %>%
+      rvest::html_element(".song-infoboard") %>%
+      rvest::html_element(".detail") %>%
+      rvest::html_text() %>%
+      stringr::str_split("\\n") %>%
+      unlist() %>%
+      purrr::pluck(4) %>%
+      stringr::str_extract_all(pattern = "[\\d,/]+") %>%
+      unlist()
+
+    data.frame(
+      df[itr, ],
+      lyric = lyric_body,
+      released = info[1],
+      page_viewed = info[2]
+    ) %>%
+      dplyr::mutate(
+        released = lubridate::as_date(.data$released),
+        page_viewed = stringr::str_remove_all(.data$page_viewed, ",") %>%
+          unlist() %>%
+          as.numeric()
+      ) %>%
+      readr::write_csv(file, append = TRUE)
+  })
+  return(invisible(file))
 }
--- README.md
+++ README.md
@@ -14,9 +14,21 @@ remotes::install_github("paithiov909/aznyan")
 ```r
 library(aznyan)
 
-tbl <-
+csv_file <-
   aznyan::get_lyrics_list("23729") |> 
-  aznyan::get_lyrics()
+  aznyan::get_lyrics("23729.csv")
+  
+tbl <-
+  readr::read_csv(csv_file, col_names = F, col_types = "cccc___cDn") |> 
+  dplyr::rename(
+    title = X1,
+    artist = X2,
+    lyricist = X3,
+    composer = X4,
+    text = X8,
+    released = X9,
+    page_view = X10
+  )
 ```
 
 ## License
